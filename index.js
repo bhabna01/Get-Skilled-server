4527ac5b2068333f04caa8c62490a8eacababe94
bhabna01 <abierhoque01@gmail.com>
2022-10-25 17:01:29 +0600

    4th commit

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
 app.get('/', (req, res) => {
     res.send('Course API Running');
 
 });
 app.get('/course-categories', (req, res) => {
     res.send(categories);
+})
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+
+
+    const categoryCourses = courses.filter(n => n.category_id == id)
+    res.send(categoryCourses);
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(n => n._id == id)
+    res.send(selectedCourses);
+
 })
 app.listen(port, () => {
 
