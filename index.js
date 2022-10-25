c2a0dc7458c5c719f6c03e4176d0fb8bbe07a174
bhabna01 <abierhoque01@gmail.com>
2022-10-25 16:07:58 +0600

    3rd commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+const categories = require('./data/categories.json')
 app.get('/', (req, res) => {
     res.send('Course API Running');
 
 });
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+})
 app.listen(port, () => {
 
 
