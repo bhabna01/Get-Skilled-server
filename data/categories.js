f754959d13adf8827f55c289a967e145573a6e55
bhabna01 <abierhoque01@gmail.com>
2022-10-25 14:33:23 +0600

    2nd commit

---
+++ data/categories.js
@@ -0,0 +1,34 @@
+[
+    {
+        "id": "01",
+        "name": "C"
+    },
+    {
+        "id": "02",
+        "name": "C++"
+    },
+    {
+        "id": "03",
+        "name": "C#"
+    },
+    {
+        "id": "04",
+        "name": "Python"
+    },
+    {
+        "id": "05",
+        "name": "Java"
+    },
+    {
+        "id": "06",
+        "name": "R language"
+    },
+    {
+        "id": "07",
+        "name": "Typescript"
+    },
+    {
+        "id": "08",
+        "name": "PHP"
+    }
+]
